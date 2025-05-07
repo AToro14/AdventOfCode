6d5719b291e6a4843806d200d9ab1e91de09a71d
Anthony Toro <spikerspaz@gmail.com>
2025-05-06 20:47:28 -0700

    Adding test files for CI Pipeline and a requirements file

---
+++ 2024/test_day/dayXX.py
@@ -0,0 +1,13 @@
+# Day XX Test for Setting Up CI Pipeline
+
+def solve_part1(a, b):
+    return (a ** b) - (b ** a)
+
+def solve_part2(a, b):
+    return (a + b) // (b - a)
+
+arg_1 = 2
+arg_2 = 5   
+
+print(solve_part1(arg_1, arg_2))
+print(solve_part2(arg_1, arg_2))
---
+++ 2024/test_day/test_dayXX.py
@@ -0,0 +1,12 @@
+from .dayXX import solve_part1, solve_part2
+
+EXAMPLE_ARG_1 = 2
+EXAMPLE_ARG_2 = 5
+
+def test_dayXX_part1():
+    """Test Day XX Part 1 with example inputs."""
+    assert solve_part1(EXAMPLE_ARG_1, EXAMPLE_ARG_2) == 7
+    
+def test_dayXX_part2():
+    """Test Day XX Part 2 with example inputs."""
+    assert solve_part2(EXAMPLE_ARG_1, EXAMPLE_ARG_2) == 2
