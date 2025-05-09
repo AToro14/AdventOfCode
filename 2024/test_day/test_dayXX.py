5a743270bce829dd24ae08c9ff32e5832648f328
Anthony Toro <spikerspaz@gmail.com>
2025-05-08 19:39:10 -0700

    Added solve_part3() concurrently with its test function; TDD

--- 2024/test_day/test_dayXX.py
+++ 2024/test_day/test_dayXX.py
@@ -10,3 +10,7 @@ def test_dayXX_part1():
 def test_dayXX_part2():
     """Test Day XX Part 2 with example inputs."""
     assert solve_part2(EXAMPLE_ARG_1, EXAMPLE_ARG_2) == 2
+    
+def test_dayXX_part3():
+    """Test Day XX Part 3 with example inputs."""
+    assert solve_part3(EXAMPLE_ARG_1, EXAMPLE_ARG_2) == 7
