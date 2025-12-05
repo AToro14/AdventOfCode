b618f59c1a7b3987e87ac0db00bbfdd5f8df65a0
Anthony Toro <AToro14@users.noreply.github.com>
2025-12-04 20:19:24 -0800

    Solution to Part 1 verified

--- 2025/d1.py
+++ 2025/d1.py
@@ -1,7 +1,7 @@
 print("Hello world!")
 
 combo_lock = [x for x in range(0, 100)] 
-position = 0
+position = 50
 password = 0
 
 print(combo_lock)
@@ -14,22 +14,41 @@ def find_direction(rotation):
     print(rotation[0])
     return rotation[0]
 
-def find_steps(rotation):
+def find_clicks(rotation):
     print(rotation[1:])
     return int(rotation[1:])
 
-def track_zero(position):
+def track_zero(position, password):
     if position == 0:
         password += 1
-    return
+    print('track_zero() password', password)
+    return password
+
+def turn_dial(direction, clicks, position):
+    if direction == 'L':
+        position = (position - clicks) % 100
+    elif direction == 'R':
+        position = (position + clicks) % 100
+    else:
+        print(direction, 'is not a valid direction')
+    print('turn_dial() position', position)
+    return position
 
 # Main loop
-for line in lines[0:4]:
+count = 0
+print('Current position', position)
+print('Current password', password)
+for line in lines:
+    count += 1
     line = line.strip()
-    print(line)
-    find_direction(line)
-    find_steps(line)
-    position += find_steps(line)
-    print('Current position', position)
-
+    #print(line)
+    #find_direction(line)
+    #find_clicks(line)
+    position = turn_dial(find_direction(line), find_clicks(line), position)
+    #position += find_steps(line)
+    password = track_zero(position, password)
+    #print('Current position', position)
+    print('Current password', password)
+    
 input_file.close()
+print('Total lines', count)
