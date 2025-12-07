3bbbe61708f6baa232626ce5e6e2ff3aac65a6bd
Anthony Toro <AToro14@users.noreply.github.com>
2025-12-07 11:20:07 -0800

    Completed Day 1 Part 2

--- 2025/d1.py
+++ 2025/d1.py
@@ -1,11 +1,12 @@
 print("Hello world!")
 
 combo_lock = [x for x in range(0, 100)] 
-position = 50
+last_position = 50
+current_position = last_position
 password = 0
 
 print(combo_lock)
-print('At position ', combo_lock[position])
+print('At position ', combo_lock[current_position])
 
 input_file = open('d1_input.txt', 'r')
 lines = input_file.readlines()
@@ -18,13 +19,32 @@ def find_clicks(rotation):
     print(rotation[1:])
     return int(rotation[1:])
 
-def track_zero(position, password):
+def track_landed_zero(position, password, clicks):
     if position == 0:
         password += 1
     print('track_zero() password', password)
     return password
 
-def turn_dial(direction, clicks, position):
+def track_passed_zeroes(direction, clicks, position, password, current_position):
+    print('track_passed_zeroes() starting with', password, 'zeroes')
+    if direction == 'L':
+        if position < (clicks % 100) and not position == 0:
+            password += 1
+            print('added 1 more going L for total of', password)
+    elif direction == 'R':
+        print('(position + clicks) % 100 =', (position + clicks) % 100, 'and (100 - position)', (100 - position))
+        if (100 - position) < (clicks % 100) and not position == 0:
+            password += 1
+            print('added 1 more going R for total of', password)
+    if clicks >= 100:
+        password += (clicks // 100)
+
+    print('adding more for total of', password)
+    return password
+
+#def track_all_zeroes(direction, clicks, position, password):
+
+def turn_dial(direction, clicks, position, password):
     if direction == 'L':
         position = (position - clicks) % 100
     elif direction == 'R':
@@ -34,9 +54,11 @@ def turn_dial(direction, clicks, position):
     print('turn_dial() position', position)
     return position
 
+
 # Main loop
 count = 0
-print('Current position', position)
+print('Current position', current_position)
+print('Last position', last_position)
 print('Current password', password)
 for line in lines:
     count += 1
@@ -44,10 +66,17 @@ for line in lines:
     #print(line)
     #find_direction(line)
     #find_clicks(line)
-    position = turn_dial(find_direction(line), find_clicks(line), position)
+    clicks = find_clicks(line)
+    direction = find_direction(line)
+    print('last_position', last_position)
+    print(clicks, 'clicks to the', direction)
+    current_position = turn_dial(direction, clicks, last_position, password)
     #position += find_steps(line)
-    password = track_zero(position, password)
+    print('current_position', current_position)
+    password = track_passed_zeroes(direction, clicks, last_position, password, current_position)
+    password = track_landed_zero(current_position, password, clicks)
     #print('Current position', position)
+    last_position = current_position
     print('Current password', password)
     
 input_file.close()
